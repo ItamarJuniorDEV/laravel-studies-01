7c9fc042e29972e136818684b8b7e9869d6023c2
Itamar Junior <cdajuniorf@gmail.com>
2025-01-25 04:25:04 -0300

    feat: Módulo completo de rotas.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,42 +6,8 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index()
+    public function teste($value): void
     {
-        echo 'index';
-    }
-
-    public function about()
-    {
-        echo 'about';
-    }
-
-    public function mostrarValor($valor)
-    {
-        echo "Valor enviado pela rota: $valor";
-    }
-    public function mostrarValores($valor1, $valor2)
-    {
-        echo "Valores enviado pela rota: $valor1 e $valor2";
-    }
-
-    public function mostrarValores2(Request $request, $valor1, $valor2)
-    {
-        echo "Valores enviado pela rota: $valor1 e $valor2";
-    }
-
-    public function mostrarValorOpcional($valor = null)
-    {
-        echo "Valor opcional: $valor";
-    }
-
-    public function mostrarValorOpcional2($valor1, $valor2 = 100)
-    {
-        echo "Valor opcional: $valor1 e $valor2";
-    }
-
-    public function mostrarPosts($user_id, $post_id)
-    {
-        echo "Posts do Usuário ID: $user_id e o post com ID: $post_id";
+        echo "A string final é: " . $this->cleanUpperCaseString($value);
     }
 }
